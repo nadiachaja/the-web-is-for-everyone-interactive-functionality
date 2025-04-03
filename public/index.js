a6894a65162dbfe4347d610355b89c26e7b5839c
nadiachaja <nadiagaja13@gmail.com>
2025-04-03 13:34:51 +0200

    #10 #8 overige code verwijderd en kleur aangepast

--- public/index.js
+++ public/index.js
@@ -10,20 +10,6 @@ if ('fetch' in window && 'DOMParser' in window) {
         form.addEventListener('submit', async function (event) {
             event.preventDefault()
 
-            // const loadingWrapper = document.createElement("div");
-            // loadingWrapper.className = "wrapper-loading";
-            // const loadingOne = document.createElement("span");
-            // loadingOne.className = "loading-one";
-            // const loadingTwo = document.createElement("span");
-            // loadingTwo.className = "loading-two";
-            // const loadingThree = document.createElement("span");
-            // loadingThree.className = "loading-three";
-
-            // loadingWrapper.appendChild(loadingOne);
-            // loadingWrapper.appendChild(loadingTwo);
-            // loadingWrapper.appendChild(loadingThree);
-
-            // form.closest('.card').appendChild(loadingWrapper);
             form.classList.add('is-loading')
 
 
@@ -49,68 +35,4 @@ if ('fetch' in window && 'DOMParser' in window) {
             })
         })
     })
-
-
-
-    // document.addEventListener('submit', async function (event) {
-
-    //     // Hou in een variabele bij welk formulier dat was
-    //     const form = event.target
-
-    //     // Als dit formulier geen data-enhanced attribuut heeft, doe dan niks
-    //     // Dit doen we, zodat we sommige formulieren op de pagina kunnen 'enhancen'
-    //     // Data attributen mag je zelf verzinnen; dit is dus niet iets speciaals
-    //     // https://developer.mozilla.org/en-US/docs/Learn_web_development/Howto/Solve_HTML_problems/Use_data_attributes
-    //     if (!form.hasAttribute('data-enhanced')) {
-    //         return
-    //     }
-
-    //     // Voorkom de standaard submit van de browser
-    //     // Let op: hiermee overschrijven we de default Loading state van de browser...
-    //     event.preventDefault()
-
-    //     form.classList.add('loading');
-
-    //     const loadingWrapper = document.createElement("div");
-    //     loadingWrapper.className = "wrapper-loading";
-    //     const loadingOne = document.createElement("span");
-    //     loadingOne.className = "loading-one";
-    //     const loadingTwo = document.createElement("span");
-    //     loadingTwo.className = "loading-two";
-    //     const loadingThree = document.createElement("span");
-    //     loadingThree.className = "loading-three";
-
-    //     loadingWrapper.appendChild(loadingOne);
-    //     loadingWrapper.appendChild(loadingTwo);
-    //     loadingWrapper.appendChild(loadingThree);
-
-
-
-    //     // Doe een fetch naar de server, net als hoe de browser dit normaal zou doen
-    //     // Gebruik daarvoor het action en method attribuut van het originele formulier
-    //     // Inclusief alle formulierelementen
-    //     await fetch(form.action, {
-    //         method: form.method,
-    //         body: new URLSearchParams(new FormData(form))
-    //     }).then((response) => {
-    //         if (response.statusText == 'OK') {
-    //             form.querySelector('button').classList.toggle('is-saved')
-    //         }
-    //     })
-
-    //     // De server redirect op de normale manier, en geeft HTML terug
-    //     // (De server weet niet eens dat deze fetch via client-side JavaScript gebeurde)
-    //     // const responseText = await response.text()
-
-
-
-    //     // Normaal zou de browser die HTML parsen en weergeven, maar daar moeten we nu zelf iets mee
-    //     // Parse de nieuwe HTML en maak hiervan een nieuw Document Object Model in het geheugen
-    //     // const parser = new DOMParser()
-    //     // const responseDOM = parser.parseFromString(responseText, 'text/html')
-
-    //     // Show liked/unlinked state on success
-
-
-    // })
 }
